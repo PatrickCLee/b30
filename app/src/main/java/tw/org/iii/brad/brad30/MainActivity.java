1cc1383880eb6c6aec2c3bdbf2b148ea147ed281
iii <zeltkimo+github@gmail.com>
2020-04-12 16:04:43 +0800

    fin

---
+++ app/src/main/java/tw/org/iii/brad/brad30/MainActivity.java
@@ -0,0 +1,112 @@
+package tw.org.iii.brad.brad30;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private MyAdapter myAdapter;
+    private LinkedList<Member> members = new LinkedList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createMemeber();
+        listView = findViewById(R.id.listView);
+        initListView();
+    }
+
+    private void createMemeber(){
+        for(int i = 0; i<4; i++){
+            Member member = new Member("Meow" + i);
+            members.add(member);
+        }
+    }
+
+    private void initListView(){
+        myAdapter = new MyAdapter();
+        listView.setAdapter(myAdapter);
+    }
+
+    public void at1(View view){
+        for(Member member : members){
+            Log.v("brad",member.getName() + ":" + member.getVIP());
+        }
+    }
+
+    public void at2(View view) {
+        Member member = new Member("BRU" + (int)(Math.random()*100));
+        members.add(member);
+        myAdapter.notifyDataSetChanged();
+    }
+
+    private class MyAdapter extends BaseAdapter{
+        private LayoutInflater inflater;
+
+        MyAdapter(){
+            inflater = getLayoutInflater();
+        }
+
+        @Override
+        public int getCount() {
+            return members.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return members.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final int pos = position;
+            View itemView = inflater.inflate(R.layout.item, null);
+
+            TextView textView = itemView.findViewById(R.id.item_tv);
+            Button btn = itemView.findViewById(R.id.item_btn);
+
+            textView.setText(members.get(position).getName());
+            btn.setText(position + "");
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this,
+                            members.get(pos).getName(),         //因position是變數
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            SwitchCompat switchCompat = itemView.findViewById(R.id.item_isVip);
+            switchCompat.setChecked(members.get(pos).getVIP());                         //因為switch會因為按下at2後重置為false
+            switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    members.get(pos).setVIP(isChecked);
+                }
+            });
+
+            return itemView;
+        }
+    }
+}
